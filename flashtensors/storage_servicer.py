6742d352fae810d80c263adf62b926e9ecfcbd9a
leoheuler <eulerai271@gmail.com>
2025-11-11 18:27:23 -0800

    Add Copyright header to modified and adapted files. Also Add Apache 2.0 compliant header to the top of the repository README.md

--- flashtensors/storage_servicer.py
+++ flashtensors/storage_servicer.py
@@ -1,3 +1,4 @@
+# Modified from: https://github.com/ServerlessLLM/ServerlessLLM/blob/main/sllm_store/sllm_store/server.py
 # ---------------------------------------------------------------------------- #
 #  ServerlessLLM                                                               #
 #  Copyright (c) ServerlessLLM Team 2024                                       #
@@ -20,7 +21,8 @@ import asyncio
 import grpc
 from .proto import storage_pb2_grpc, storage_pb2
 from .utils import init_logger
-from ._checkpoint_store import (CheckpointStore, MemCopyChunk)
+from ._checkpoint_store import CheckpointStore, MemCopyChunk
+
 
 class StorageServicer(storage_pb2_grpc.StorageServicer):
 
@@ -90,9 +92,7 @@ class StorageServicer(storage_pb2_grpc.StorageServicer):
                 return storage_pb2.LoadModelResponse()
 
             gpu_memory_handles = {
-                device_uuid: [
-                    handle.cuda_ipc_handle for handle in handle_list.handles
-                ]
+                device_uuid: [handle.cuda_ipc_handle for handle in handle_list.handles]
                 for device_uuid, handle_list in request.handles.items()
             }
 
@@ -127,9 +127,7 @@ class StorageServicer(storage_pb2_grpc.StorageServicer):
             context.set_code(grpc.StatusCode.INTERNAL)
             return storage_pb2.LoadModelResponse()
 
-        self.logger.info(
-            f"LoadModel: success {model_path} with target {device_type}"
-        )
+        self.logger.info(f"LoadModel: success {model_path} with target {device_type}")
         return storage_pb2.LoadModelResponse(model_path=model_path)
 
     async def ConfirmModel(self, request, context):
@@ -156,11 +154,15 @@ class StorageServicer(storage_pb2_grpc.StorageServicer):
                     )
                     return storage_pb2.ConfirmModelResponse(model_path=model_path)
                 elif ret == 1:
-                    self.logger.warning(f"Model {model_path} replica {replica_uuid} is interrupted")
+                    self.logger.warning(
+                        f"Model {model_path} replica {replica_uuid} is interrupted"
+                    )
                     context.set_code(grpc.StatusCode.INTERNAL)
                     return storage_pb2.ConfirmModelResponse()
                 else:
-                    self.logger.warning(f"Confirm model failed with ret={ret}, retry {i + 1}")
+                    self.logger.warning(
+                        f"Confirm model failed with ret={ret}, retry {i + 1}"
+                    )
             except Exception as e:
                 self.logger.error(f"Exception in wait_model_in_gpu: {e}")
                 if i == 19:  # Last retry
@@ -168,9 +170,7 @@ class StorageServicer(storage_pb2_grpc.StorageServicer):
 
             await asyncio.sleep(0.1)  # Longer sleep for stability
 
-        self.logger.error(
-            f"Confirm model {model_path} replica {replica_uuid} failed"
-        )
+        self.logger.error(f"Confirm model {model_path} replica {replica_uuid} failed")
         context.set_code(grpc.StatusCode.INTERNAL)
         return storage_pb2.ConfirmModelResponse()
 
